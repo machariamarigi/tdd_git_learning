8e8c57137e6a32b3e62b99e59b21af7e1f0fcbef
machariamarigi <machariamarigi@gmail.com>
2017-04-09 10:28:42 +0300

    Test to ensure only numbers are added

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -16,9 +16,21 @@ class TestCalculator(unittest.TestCase):
         self.calc = Calculator()
 
     def test_calculator_add_works(self):
-        """Funtion test if our function works"""
+        """test if our add function works"""
         result = self.calc.add(2, 2)
         self.assertEqual(4, result)
+
+    def test_calculator_add_not_numbers(self):
+        """Test to see if our add function adds only numbers"""
+        self.assertRaises(ValueError, self.calc.add, "mash", "cool")
+
+    def test_calculator_add_not_numbers_val1(self):
+        """Test to see if our add function adds only numbers"""
+        self.assertRaises(ValueError, self.calc.add, "mash", 1)
+
+    def test_calculator_add_not_numbers_val2(self):
+        """Test to see if our add function adds only numbers"""
+        self.assertRaises(ValueError, self.calc.add, 2, "cool")
 
 if __name__ == '__main__':
     unittest.main()
