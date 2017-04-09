8cbe003079183fd733198c41c28139511ef44c75
machariamarigi <machariamarigi@gmail.com>
2017-04-09 10:05:17 +0300

    Calculator module first build passes working test

---
+++ app/calculator.py
@@ -0,0 +1,15 @@
+"""This module contains the calculator logic application encapsulted in the
+    Calculator class
+"""
+
+
+class Calculator(object):
+    """This class contains methods that accomplish arithmetic maipulation of
+        numbers
+    """
+
+    def add(self, val1, val2):
+        """Method that performs the adding arithmetic operation of 2 numbers
+            passed in the th argument
+        """
+        return val1 + val2
